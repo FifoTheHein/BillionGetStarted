018eccf74bb37bb664e142cca6d6b242f255719a
Heinrich Nel <hein@codecollective.com>
2020-04-07 17:42:20 +0200

    Allow nullable fields on airport type

--- GraphQLAPI/GraphQL/Types/AirportType.cs
+++ GraphQLAPI/GraphQL/Types/AirportType.cs
@@ -10,8 +10,8 @@ namespace GraphQLAPI.GraphQL.Types
             Field(a => a.Title).Description("The name of the Airport");
             Field(a => a.Lat);
             Field(a => a.Lon);
-            Field(a => a.IATACode);
-            Field(a => a.ICAOCode);
+            Field(a => a.IATACode, nullable:true);
+            Field(a => a.ICAOCode, nullable:true);
             Field(a => a.CityID, type: typeof(IdGraphType));
             Field<CityType>("City", "The city the airport is in");
         }
